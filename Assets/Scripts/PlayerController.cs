abb90db28af8cce9726b7bf7fa61e454952e547a
Jim Valenti <slimjim96@gmail.com>
2025-12-04 00:02:11 -0500

    Added jumping.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float jumpForce = 0.1f;
+    [SerializeField] float maxJumpHeight = 2f;
     InputAction moveAction;
     Rigidbody2D rb;
     // Start is called before the first frame update
@@ -19,9 +22,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector2 moveVector = moveAction.ReadValue<Vector2>();
-        if(moveVector.x < 0)
+        if (moveVector.x < 0)
+        {
+            //write to unity console
+            Debug.Log("Left");
             rb.AddTorque(torqueAmount);
+        }
         else if(moveVector.x > 0)       
             rb.AddTorque(-torqueAmount);
+
+        //Add a jump when the player presses the up arrow or W key
+        if (moveVector.y > 0)
+        {
+            Debug.Log("Jump");
+            //limit jump to when player is on the ground and to a certain height
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        }
     }
 }
